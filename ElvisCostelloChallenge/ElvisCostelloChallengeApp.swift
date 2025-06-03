5d3f9b3c53ceaa4a8ed5a5bcacd4d33f4d4ebe03
Garychamp <garyhoare18@gmail.com>
2025-06-03 19:41:49 +0100

    Initial Commit

---
+++ ElvisCostelloChallenge/ElvisCostelloChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloChallengeApp.swift
+//  ElvisCostelloChallenge
+//
+//  Created by Gary Hoare on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
